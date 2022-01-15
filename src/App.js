79489bc27cdd216426260a1e37eeb05e6b0727a1
hihumikan <musecharo@gmail.com>
2022-01-15 16:26:53 +0900

    リストの表示

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
-
+import RandomTopping from "./components/RandomTopping";
 function App() {
   return (
     <div className="App">
       <>
         <Header />
+        <RandomTopping />
       </>
     </div>
   );
