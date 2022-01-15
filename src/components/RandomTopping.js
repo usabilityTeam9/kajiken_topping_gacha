cbbe797072fa597e6f91446efc78b3046051b0c3
hihumikan <musecharo@gmail.com>
2022-01-15 16:32:40 +0900

    Itemの大きさの変更

--- src/components/RandomTopping.js
+++ src/components/RandomTopping.js
@@ -1,9 +1,12 @@
+import * as React from "react";
 import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
+
 import { styled } from "@mui/material/styles";
+
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: theme.spacing(4),
   textAlign: "center",
   color: theme.palette.text.secondary,
 }));
@@ -11,7 +14,7 @@ const Item = styled(Paper)(({ theme }) => ({
 export default function RandomTopping() {
   return (
     <div>
-      <Stack spacing={2}>
+      <Stack direction="column" spacing={2}>
         <Item>Item 1</Item>
         <Item>Item 2</Item>
         <Item>Item 3</Item>
