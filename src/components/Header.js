cc75fe1ba061897535d7280461a768e7f22a9ce3
hihumikan <musecharo@gmail.com>
2022-01-15 16:24:17 +0900

    Headerの暫定

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,48 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+
+function appBar(label) {
+  return (
+    <Toolbar>
+      <IconButton
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+      ></IconButton>
+      <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+        {label}
+      </Typography>
+    </Toolbar>
+  );
+}
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#1976d2",
+    },
+  },
+});
 
 function Header() {
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            ></IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Kajiken_topping_Gacha
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
+      <Stack spacing={1} sx={{ flexGrow: 1 }}>
+        <ThemeProvider theme={darkTheme}>
+          <AppBar position="relative" color="primary">
+            {appBar("Kajiken_topping_gacha")}
+          </AppBar>
+          <Button></Button>
+        </ThemeProvider>
+      </Stack>
     </>
   );
 }
