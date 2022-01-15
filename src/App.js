d22051a73c591404095139ef433704ddd375d77f
hihumikan <musecharo@gmail.com>
2022-01-15 15:04:12 +0900

    :sparkles: (MUI) muiの追加

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Button from "@mui/material/Button";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </>
+    </div>
+  );
 }
 
 export default App;
