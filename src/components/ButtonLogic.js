c558f5fc9eb2f40fa3c399644ccf8159c6f961d4
hihumikan <musecharo@gmail.com>
2022-01-18 01:45:00 +0900

    処理の追加

--- src/components/ButtonLogic.js
+++ src/components/ButtonLogic.js
@@ -22,6 +22,24 @@ export default function ButtonLogic() {
     "カレー",
   ]);
   const [toppinglist250, setTopping250] = useState(["チャーシュー"]);
+
+  // toppinglist20, toppinglist50, toppinglist100, toppinglist250からLevel別で選ぶ
+  function Randompick(level) {
+    if (level === 3) {
+      setPossession_money(possession_money - 250);
+      return toppinglist250[Math.floor(Math.random() * toppinglist250.length)];
+    } else if (level === 2) {
+      setPossession_money(possession_money - 100);
+      return toppinglist100[Math.floor(Math.random() * toppinglist100.length)];
+    } else if (level === 1) {
+      setPossession_money(possession_money - 50);
+      return toppinglist50[Math.floor(Math.random() * toppinglist50.length)];
+    } else if (level === 0) {
+      setPossession_money(possession_money - 20);
+      return toppinglist20[Math.floor(Math.random() * toppinglist20.length)];
+    }
+  }
+  /*
   function Randompick(level) {
     let toppings = [];
     if (level === 3) {
@@ -33,23 +51,58 @@ export default function ButtonLogic() {
     } else if (level === 0) {
       toppings = [toppinglist20];
     }
-    return Listpick(toppings[Math.floor(Math.random() * toppings.length)]);
+
+    let index = Math.floor(Math.random() * toppings.length);
+
+    if (index === 0) {
+      setPossession_money(possession_money - 20);
+    } else if (index === 1) {
+      setPossession_money(possession_money - 50);
+    } else if (index === 2) {
+      setPossession_money(possession_money - 100);
+    } else if (index === 3) {
+      setPossession_money(possession_money - 250);
+    }
+    console.log(possession_money);
+    return Listpick(toppings[index]);
   }
   function Listpick(list) {
     const random = Math.floor(Math.random() * list.length);
-    setCount(random);
     return list[random];
   }
-  function handleClick() {
-    console.log("クリックされました");
+  */
 
-    Randompick(Math.floor(Math.random() * 4));
+  //ボタンを押した時の処理
+  function handleClick() {
+    //possesion_moneyが0以上
+    while (possession_money > 0) {
+      setPossession_money(possession_money - 20);
+      /*
+      if (possession_money >= 250) {
+        setPossession_stock(possession_stock.concat(Randompick(3)));
+        setPossession_money(possession_money - 20);
+      } else if (possession_money >= 100) {
+        setPossession_stock(possession_stock.concat(Randompick(2)));
+        setPossession_money(possession_money - 20);
+      } else if (possession_money >= 50) {
+        setPossession_stock(possession_stock.concat(Randompick(1)));
+        setPossession_money(possession_money - 20);
+      } else if (possession_money >= 20) {
+        setPossession_stock(possession_stock.concat(Randompick(0)));
+        setPossession_money(possession_money - 20);
+      } else {
+        break;
+      }*/
+      console.log(possession_stock);
+      console.log(possession_money);
+    }
   }
 
   return (
     <div>
       <button
         onClick={() => {
+          setCount(count + 1);
           handleClick();
         }}
       >
